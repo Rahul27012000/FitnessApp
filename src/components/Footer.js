bc3ffd9f3f2f16bd1e7625487d25cc088a70339e
Rahul Maheshwari <rahul.maheshwari@graphenesvc.com>
2022-06-19 00:15:04 +0530

    Fitness App Ready!

---
+++ src/components/Footer.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import { Box, Stack, Typography } from '@mui/material';
+
+const Footer = () => (
+    <Typography variant="h5" sx={{ fontSize: { lg: '28px', xs: '20px' } }} mt="41px" textAlign="center" pb="40px">Made with ❤️ by Rahul Maheshwari</Typography>
+
+);
+
+export default Footer;
